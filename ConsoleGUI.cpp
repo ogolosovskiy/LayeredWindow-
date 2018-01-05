eaa9d96614468f1eae14a717b24ebef14912a0fb
Oleg Golosovskiy <ogolosovskiy@gmail.com>
2018-01-05 22:37:05 +0300

    SFML remove

--- ConsoleGUI.cpp
+++ ConsoleGUI.cpp
@@ -1,52 +1,72 @@
-#include <SFML/Graphics.hpp>
 
 #if _WIN32_WINNT < 0x0501
 #define _WIN32_WINNT 0x0501
 #endif
 #include <windows.h>
 
-// Set part of the window that can be clicked by removing fully transparent pixels from the region
-void setShape(HWND hWnd, const sf::Image& image)
+#define CURRENT_WND_CLASS L"WndClass_AAA"
+
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
-	const sf::Uint8* pixelData = image.getPixelsPtr();
-	HRGN hRegion = CreateRectRgn(0, 0, image.getSize().x, image.getSize().y);
-
-	for (unsigned int y = 0; y < image.getSize().y; y++)
+	switch (uMsg)
 	{
-		for (unsigned int x = 0; x < image.getSize().x; x++)
-		{
-			if (pixelData[y * image.getSize().x * 4 + x * 4 + 3] == 0)
-			{
-				HRGN hRegionPixel = CreateRectRgn(x, y, x + 1, y + 1);
-				CombineRgn(hRegion, hRegion, hRegionPixel, RGN_XOR);
-				DeleteObject(hRegionPixel);
-			}
-		}
+	case WM_QUIT:
+	case WM_DESTROY:
+	case WM_CLOSE:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, uMsg, wParam, lParam);
 	}
-
-	SetWindowRgn(hWnd, hRegion, true);
-	DeleteObject(hRegion);
+	return 0;
 }
 
 int main()
 {
 
-	// Change this if you want to apply transparency on top of the one already in the image
-	const sf::Uint8 alpha = 0xff;
+	WNDCLASSEX wcex; /* Structure needed for creating Window */
+	MSG msg;
 
-	// Load an image with transparent parts that will be drawn to the screen
-	sf::Image image;
-	image.loadFromFile("image.png");
+	ZeroMemory(&wcex, sizeof(WNDCLASSEX));
+	ZeroMemory(&msg, sizeof(MSG));
+	HINSTANCE hInstance = GetModuleHandle(NULL);
+
+	wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	wcex.hCursor = LoadCursor(hInstance, IDC_ARROW);
+	wcex.hIcon = LoadIcon(hInstance, IDI_APPLICATION);
+	wcex.hInstance = hInstance;
+	wcex.lpfnWndProc = WndProc;
+	wcex.lpszClassName = CURRENT_WND_CLASS;
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.cbWndExtra = 0;
+	wcex.cbClsExtra = 0;
+	wcex.lpszMenuName = NULL;
+
+	if (!RegisterClassEx(&wcex)) {
+		return -1;
+	}
+
+
+	unsigned int width = 400;
+	unsigned int height = 200;
 
 	// Create the window and center it on the screen
-	sf::RenderWindow window(sf::VideoMode(image.getSize().x, image.getSize().y, 32), "Transparent Window", sf::Style::None);
-	window.setPosition(sf::Vector2i((sf::VideoMode::getDesktopMode().width - image.getSize().x) / 2,
-		(sf::VideoMode::getDesktopMode().height - image.getSize().y) / 2));
-
-	HWND hWnd = window.getSystemHandle();
-
+	HWND hWnd = CreateWindowEx(	WS_EX_LAYERED | // Layered Windows
+								WS_EX_TRANSPARENT | // Don't hittest this window
+								WS_EX_TOPMOST | WS_EX_TOOLWINDOW,
+								CURRENT_WND_CLASS, 
+								L"Title",
+								WS_POPUP | WS_VISIBLE, 
+								200, 200, width, height, 
+								NULL, // hWndParent
+								NULL, // hMenu
+								hInstance,
+								NULL);
+	
 	// Full transparent pixels should not be part of the window
-	setShape(hWnd, image);
+	//setShape(hWnd, image);
 
 	// Create a DC for our bitmap
 	HDC hdcWnd = GetDC(hWnd);
@@ -61,38 +81,52 @@ int main()
 	BITMAPINFO bmi;
 	ZeroMemory(&bmi, sizeof(BITMAPINFO));
 	bmi.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
-	bmi.bmiHeader.biWidth = image.getSize().x;
-	bmi.bmiHeader.biHeight = image.getSize().y;
+	bmi.bmiHeader.biWidth = width;
+	bmi.bmiHeader.biHeight = height;
 	bmi.bmiHeader.biPlanes = 1;
 	bmi.bmiHeader.biBitCount = 32;         // four 8-bit components
 	bmi.bmiHeader.biCompression = BI_RGB;
-	bmi.bmiHeader.biSizeImage = image.getSize().x * image.getSize().y * 4;
+	bmi.bmiHeader.biSizeImage = width * height * 4;
 	HBITMAP hbitmap = CreateDIBSection(hdc, &bmi, DIB_RGB_COLORS, &pvBits, NULL, 0x0);
 	SelectObject(hdc, hbitmap);
 
 	// Copy the pixels from the image to the bitmap (but pre-multiply the alpha value)
-	const sf::Uint8* pixelData = image.getPixelsPtr();
-	for (unsigned int y = 0; y < image.getSize().y; ++y)
+	for (unsigned int y = 0; y < height; ++y)
 	{
-		for (unsigned int x = 0; x < image.getSize().x; ++x)
+		for (unsigned int x = 0; x < width; ++x)
 		{
-			float fAlphaFactor = (float)pixelData[y * image.getSize().x * 4 + x * 4 + 3] / (float)0xff;
-			((UINT32 *)pvBits)[x + (image.getSize().y - y - 1) * image.getSize().x]
-				= (pixelData[y * image.getSize().x * 4 + x * 4 + 3] << 24) |                        //0xaa000000
-				((UCHAR)(pixelData[y * image.getSize().x * 4 + x * 4] * fAlphaFactor) << 16) |     //0x00rr0000
-				((UCHAR)(pixelData[y * image.getSize().x * 4 + x * 4 + 1] * fAlphaFactor) << 8) |  //0x0000gg00
-				((UCHAR)(pixelData[y * image.getSize().x * 4 + x * 4 + 2] * fAlphaFactor));      //0x000000bb
+			float fAlphaFactor = 0;
+
+			if (y>100)
+				fAlphaFactor = 1;
+
+			int alpha = 0;
+			if (x>100)
+				alpha = 255;
+
+			((UINT32 *)pvBits)[x + (height - y - 1) * width]
+				= (alpha << 24) |                        //0xaa000000
+				((UCHAR)(150 * fAlphaFactor) << 16) |     //0x00rr0000
+				((UCHAR)(150 * fAlphaFactor) << 8) |  //0x0000gg00
+				((UCHAR)(150 * fAlphaFactor));      //0x000000bb
+
+//			float fAlphaFactor = (float)pixelData[y * image.getSize().x * 4 + x * 4 + 3] / (float)0xff;
+//			((UINT32 *)pvBits)[x + (image.getSize().y - y - 1) * image.getSize().x]
+//				= (pixelData[y * image.getSize().x * 4 + x * 4 + 3] << 24) |                        //0xaa000000
+//				((UCHAR)(pixelData[y * image.getSize().x * 4 + x * 4] * fAlphaFactor) << 16) |     //0x00rr0000
+//				((UCHAR)(pixelData[y * image.getSize().x * 4 + x * 4 + 1] * fAlphaFactor) << 8) |  //0x0000gg00
+//				((UCHAR)(pixelData[y * image.getSize().x * 4 + x * 4 + 2] * fAlphaFactor));      //0x000000bb
 		}
-	}
+	} 
 
 	// Put the image on the screen
 	POINT ptSrc = { 0, 0 };
-	SIZE sizeWnd = { (long)image.getSize().x, (long)image.getSize().y };
+	SIZE sizeWnd = { (long)width, (long)height };
 	BLENDFUNCTION bf;
 	bf.BlendOp = AC_SRC_OVER;
 	bf.BlendFlags = 0;
 	bf.AlphaFormat = AC_SRC_ALPHA;
-	bf.SourceConstantAlpha = alpha;
+	bf.SourceConstantAlpha = 255;
 	UpdateLayeredWindow(hWnd, NULL, NULL, &sizeWnd, hdc, &ptSrc, 0, &bf, ULW_ALPHA);
 
 	// Cleanup
@@ -100,17 +134,22 @@ int main()
 	DeleteDC(hdc);
 	DeleteDC(hdcWnd);
 
-	// Main loop that handles the events but does not draw anything
-	while (window.isOpen())
-	{
-		sf::Event event;
-		while (window.pollEvent(event))
-		{
-			if (event.type == sf::Event::Closed || (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape))
-				window.close();
-		}
+	BOOL bDone = FALSE;
+	while (FALSE == bDone) {
+		if (PeekMessage(&msg, hWnd, 0, 0, PM_REMOVE)) {
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
 
-		// Don't waste CPU time
-		sf::sleep(sf::milliseconds(10));
+			if (msg.message == WM_QUIT) {
+				bDone = TRUE;
+			}
+		}
+		else {
+			/* do rendering here */
+		}
 	}
+
+	DestroyWindow(hWnd);
+	UnregisterClass(CURRENT_WND_CLASS, hInstance);
+	return 0;
 }
